24a60abe97180229cf7ddb5200ca12e616d590e3
Stijn Swaanen <swaanenstijn@gmail.com>
2015-02-20 15:03:39 +0000

    UPS-1501 leave values with integer 0 in data array

--- CultureFeed/CultureFeed/Uitpas/ValueObject.php
+++ CultureFeed/CultureFeed/Uitpas/ValueObject.php
@@ -5,7 +5,7 @@ abstract class CultureFeed_Uitpas_ValueObject {
   public function toPostData() {
     $data = get_object_vars($this);
     $this->manipulatePostData($data);
-    $data = array_filter($data);
+    $data = array_filter($data, 'strlen');
 
     return $data;
   }
