e3c635bddf6e87334e2e65adc8cd2feeed16a6c4
Kristof Coomans <kristof@coomans.eu>
2015-05-05 14:27:11 +0200

    MSS-151: Add tests for adding keywords as objects

--- CultureFeed/test/EntryApi/EntryApiTest.php
+++ CultureFeed/test/EntryApi/EntryApiTest.php
@@ -49,6 +49,47 @@ class CultureFeed_EntryApiTest extends PHPUnit_Framework_TestCase {
     $this->entry->addTagToEvent($this->event, $keywords);
   }
 
+  public function testAddTagToEventWithKeywordsMixedAsObjectsAndStrings() {
+    $keywords = array(
+      new CultureFeed_Cdb_Data_Keyword('foo', true),
+      new CultureFeed_Cdb_Data_Keyword('bar', false),
+      'yet another keyword',
+    );
+
+    $this->oauthClient->expects($this->once())
+      ->method('authenticatedPostAsXml')
+      ->with(
+        'event/xyz/keywords',
+        array(
+          'keywords' => 'foo;bar;yet another keyword',
+          'visibles' => 'true;false;true',
+        )
+      )
+      ->willReturn($this->keywordsCreatedResponse());
+
+    $this->entry->addTagToEvent($this->event, $keywords);
+  }
+
+  public function invalidKeywordProvider() {
+    return array(
+      array(5),
+      array(new stdClass()),
+      array(array()),
+      array(NULL)
+    );
+  }
+
+  /**
+   * @dataProvider invalidKeywordProvider
+   *
+   * @param array $keywords
+   */
+  public function testAddTagToEventWithInvalidKeywords($keyword) {
+    $keywords = array($keyword);
+    $this->setExpectedException('InvalidArgumentException');
+    $this->entry->addTagToEvent($this->event, $keywords);
+  }
+
   private function keywordsCreatedResponse() {
     return file_get_contents(__DIR__ . '/samples/rsp-keywords-created.xml');
   }
