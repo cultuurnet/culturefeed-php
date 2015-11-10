16d0626d5873780c29b293fb7e6014e7219adc36
Sander Vleugels <sandervleugels@gmail.com>
2015-11-10 11:34:42 +0100

    UBR-347: add more properties to the culture feed coupon object

--- CultureFeed/CultureFeed/Uitpas/Event/TicketSale/Coupon.php
+++ CultureFeed/CultureFeed/Uitpas/Event/TicketSale/Coupon.php
@@ -12,6 +12,21 @@ class CultureFeed_Uitpas_Event_TicketSale_Coupon extends CultureFeed_Uitpas_Valu
   public $name;
 
   /**
+   * @var string
+   */
+  public $description;
+
+  /**
+   * @var string
+   */
+  public $validFrom;
+
+  /**
+   * @var string
+   */
+  public $validTo;
+
+  /**
    * @var CultureFeed_Uitpas_PeriodConstraint
    */
   public $exchangeConstraint;
@@ -20,6 +35,11 @@ class CultureFeed_Uitpas_Event_TicketSale_Coupon extends CultureFeed_Uitpas_Valu
    * @var CultureFeed_Uitpas_PeriodConstraint
    */
   public $buyConstraint;
+
+  /**
+   * @var CultureFeed_Uitpas_PeriodConstraint
+   */
+  public $remainingTotal;
 
   /**
    * @param CultureFeed_SimpleXMLElement $object
@@ -31,6 +51,9 @@ class CultureFeed_Uitpas_Event_TicketSale_Coupon extends CultureFeed_Uitpas_Valu
 
     $coupon->id = $object->xpath_str('id', FALSE);
     $coupon->name = $object->xpath_str('name', FALSE);
+    $coupon->description = $object->xpath_str('description', FALSE);
+    $coupon->validFrom = $object->xpath_time('validFrom');
+    $coupon->validTo = $object->xpath_time('validTo');
 
     $exchangeConstraintElement = $object->xpath('exchangeConstraint', FALSE);
     if ($exchangeConstraintElement instanceof CultureFeed_SimpleXMLElement) {
@@ -40,6 +63,11 @@ class CultureFeed_Uitpas_Event_TicketSale_Coupon extends CultureFeed_Uitpas_Valu
     $buyConstraintElement = $object->xpath('buyConstraint', FALSE);
     if ($buyConstraintElement instanceof CultureFeed_SimpleXMLElement) {
       $coupon->buyConstraint = CultureFeed_Uitpas_PeriodConstraint::createFromXML($buyConstraintElement);
+    }
+
+    $remainingTotalElement = $object->xpath('remainingTotal', FALSE);
+    if ($remainingTotalElement instanceof CultureFeed_SimpleXMLElement) {
+      $coupon->remainingTotal = CultureFeed_Uitpas_PeriodConstraint::createFromXML($remainingTotalElement);
     }
 
     return $coupon;
