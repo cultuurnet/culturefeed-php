ccc3ae1f7f0e73562fbba57da678871aed01c0b4
Kristof Coomans <kristof@2dotstwice.be>
2016-10-28 07:23:06 +0200

    III-1491: Test post data for distributionKey and price names + values

--- CultureFeed/test/uitpas/Event/CultureEventTest.php
+++ CultureFeed/test/uitpas/Event/CultureEventTest.php
@@ -15,11 +15,11 @@ class CultureFeed_Uitpas_Event_CultureEventTest extends PHPUnit_Framework_TestCa
     $this->assertSame('5c9c73d3-e82f-e7b3-44161e6e3802e64f', $event->organiserId);
 
     $distributionKey200 = new CultureFeed_Uitpas_DistributionKey();
-    $distributionKey200->id = 200;
+    $distributionKey200->id = '200';
     $distributionKey200->name = 'Distribution key 200';
 
     $distributionKey201 = new CultureFeed_Uitpas_DistributionKey();
-    $distributionKey201->id = 201;
+    $distributionKey201->id = '201';
     $distributionKey201->name = 'Distribution key 201';
 
     $this->assertInternalType('array', $event->distributionKey);
@@ -33,4 +33,88 @@ class CultureFeed_Uitpas_Event_CultureEventTest extends PHPUnit_Framework_TestCa
     );
   }
 
+
+  public function testToPostDataBasicProperties() {
+    $event = new CultureFeed_Uitpas_Event_CultureEvent();
+
+    $event->cdbid = '9ba1b072-40ea-41b6-a66b-ac3fdf646f36';
+    $event->organiserId = '5C9C73D3-E82F-E7B3-44161E6E3802E64F';
+    $event->locationId = 'afcbe4cf-d873-468b-80f8-7fe583f45e5e';
+
+    $postData = $event->toPostData();
+
+    $this->assertSame(
+      array(
+        'cdbid' => '9ba1b072-40ea-41b6-a66b-ac3fdf646f36',
+        'locationId' => 'afcbe4cf-d873-468b-80f8-7fe583f45e5e',
+        'actorId' => '5C9C73D3-E82F-E7B3-44161E6E3802E64F',
+      ),
+      $postData
+    );
+  }
+
+  public function testToPostDataWithDistributionKeyAsString() {
+    $event = new CultureFeed_Uitpas_Event_CultureEvent();
+
+    $event->distributionKey = '200';
+
+    $postData = $event->toPostData();
+
+    $this->assertArrayHasKey('distributionKey', $postData);
+    $this->assertSame('200', $postData['distributionKey']);
+  }
+
+  public function testToPostDataWithDistributionKeysAsArrayOfObjects() {
+    $event = new CultureFeed_Uitpas_Event_CultureEvent();
+
+    $distributionKey200 = new CultureFeed_Uitpas_DistributionKey();
+    $distributionKey200->id = '200';
+    $distributionKey200->name = 'Distribution key 200';
+
+    $distributionKey201 = new CultureFeed_Uitpas_DistributionKey();
+    $distributionKey201->id = '201';
+    $distributionKey201->name = 'Distribution key 201';
+
+    $event->distributionKey = array(
+      $distributionKey200,
+      $distributionKey201,
+    );
+
+    $postData = $event->toPostData();
+
+    $this->assertArrayHasKey('distributionKey', $postData);
+    $this->assertSame(
+      array(
+        '200',
+        '201',
+      ),
+      $postData['distributionKey']
+    );
+  }
+
+  public function testPostDataPriceNamesAndValues() {
+    $event = new CultureFeed_Uitpas_Event_CultureEvent();
+
+    $event->postPriceNames = array(
+      'price 1',
+      'price 2'
+    );
+
+    $event->postPriceValues = array(
+      10.5,
+      11.6,
+    );
+
+    $postData = $event->toPostData();
+
+    $this->assertArraySubset(
+      array(
+        'price.name.1' => 'price 1',
+        'price.value.1' => 10.5,
+        'price.name.2' => 'price 2',
+        'price.value.2' => 11.6
+      ),
+      $postData
+    );
+  }
 }
