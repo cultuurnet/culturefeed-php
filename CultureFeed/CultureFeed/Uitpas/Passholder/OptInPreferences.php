251d9ba3880d52049c2c42bc8befd11fb14fd7ff
Nils Destoop <nils.destoop@gmail.com>
2018-05-11 13:46:46 +0200

    UPS-2596: Send boolean properties as true or false

--- CultureFeed/CultureFeed/Uitpas/Passholder/OptInPreferences.php
+++ CultureFeed/CultureFeed/Uitpas/Passholder/OptInPreferences.php
@@ -50,4 +50,17 @@ class CultureFeed_Uitpas_Passholder_OptInPreferences extends CultureFeed_Uitpas_
     return $preferences;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function toPostData() {
+
+    $data = get_object_vars($this);
+    foreach ($data as $key => $value) {
+      $data[$key] = $value ? 'true' : 'false';
+    }
+
+    return $data;
+  }
+
 }
