d9081c917c9a2ced95e4341217df279351b418ae
Bert Ramakers <bert@madewithlove.be>
2019-11-21 15:13:34 +0100

    Remove now unnecessary PHPUnit_Framework_TestCase alias

--- CultureFeed/test/bootstrap.php
+++ CultureFeed/test/bootstrap.php
@@ -4,8 +4,4 @@ if (file_exists('vendor/autoload.php')) {
   require_once 'vendor/autoload.php';
 }
 
-require_once 'BackwardsCompatiblePHPUnitTestCase.php';
-
 date_default_timezone_set('Europe/Brussels');
-
-class_alias(BackwardsCompatiblePHPUnitTestCase::class, 'PHPUnit_Framework_TestCase');
