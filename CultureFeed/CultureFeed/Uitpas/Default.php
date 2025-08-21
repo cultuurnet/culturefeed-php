fad74635c41bc3025955cc1ef5924112a211e404
Luc Wollants <lucwollants@gmail.com>
2025-08-21 15:31:45 +0200

    Construct activation data correctly

--- CultureFeed/CultureFeed/Uitpas/Default.php
+++ CultureFeed/CultureFeed/Uitpas/Default.php
@@ -1035,8 +1035,11 @@ class CultureFeed_Uitpas_Default implements CultureFeed_Uitpas {
 
   public function getPassholderActivationLinkChainedWithAuthorization($uitpas_number, DateTime $date_of_birth, $callback_url) {
     $c = $this->culturefeed;
+    $activation_data = new CultureFeed_Uitpas_Passholder_Query_ActivationData();
+    $activation_data->uitpasNumber = $uitpas_number;
+    $activation_data->dob = $date_of_birth;
 
-    $link = $this->getPassholderActivationLink($uitpas_number, function () use ($c, $callback_url) {
+    $link = $this->getPassholderActivationLink($activation_data, function () use ($c, $callback_url) {
       $token = $c->getRequestToken($callback_url);
 
       $auth_url = $c->getUrlAuthorize($token, $callback_url, CultureFeed::AUTHORIZE_TYPE_REGULAR, TRUE);
