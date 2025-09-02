ceeddc069505ec8dec9a905b1914676c4a4a0bf9
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-09-02 14:32:49 +0200

    add typehinting to CultureFeed_ResultSet

--- CultureFeed/CultureFeed/ResultSet.php
+++ CultureFeed/CultureFeed/ResultSet.php
@@ -1,30 +1,12 @@
 <?php
 
-/**
- * Class to represent a result set (results + total number of results).
- */
 class CultureFeed_ResultSet {
 
-  /**
-   * The total number of objects in the complete set.
-   *
-   * The result set holds a slice of the complete set.
-   *
-   * @var int
-   */
-  public $total;
+  public int $total;
 
   public array $objects;
 
-  /**
-   * Constructor for a new CultureFeed_ResultSet instance.
-   *
-   * @param integer $total
-   *   The total number of objects in the complete set.
-   * @param array $objects
-   *   The objects in the slice.
-   */
-  public function __construct($total = 0, $objects = array()) {
+  public function __construct(int $total = 0, array $objects = array()) {
     $this->total = $total;
     $this->objects = $objects;
   }
